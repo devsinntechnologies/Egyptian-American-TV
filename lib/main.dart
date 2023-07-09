9365b978c2c3ae443920b4da60d320a779668f93
UBAIDULLAH ASIM <68696581+ubaid9029@users.noreply.github.com>
2023-07-09 18:00:41 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.green,
+        primaryColor: Colors.grey,
         // secondaryHeaderColor: kPrimaryColor,
       ),
       home: AddPlayListScreen(),
